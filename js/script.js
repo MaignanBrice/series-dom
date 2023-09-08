66898c91c01feb3dcd130de97f354d56b8ed5946
Brice Maignan <mgnn.brice@pm.me>
2023-09-08 11:05:15 +0200

    Added getIdOnClick & getSerieInfoById

--- js/script.js
+++ js/script.js
@@ -6,12 +6,10 @@ async function fetchingSeriesDatas() {
 
     displaySeriesStyles(getSeriesStyle(seriesData));
     seriesData.forEach(function (serie) {
-        // displaySerie(serie);
-        displaySerieFromId(serie, 2);
-
+        displaySerie(serie);
     });
-    console.log(getSeriesIdFromStyle(seriesData, "Aventure"));
-    underlineOnClic();
+    displaySerieByStyle(seriesData);
+    getIdOnClick();
 };
 
 fetchingSeriesDatas();
@@ -22,12 +20,15 @@ function displaySerie(object) {
     let newContainer = document.createElement('div');
     let newH2 = document.createElement('h2');
     let newImg = document.createElement('img');
+    newContainer.id = 'container';
     newContainer.classList.add('serie-container');
     newH2.classList.add('serie-title');
     newContainer.append(newH2);
     newContainer.append(newImg);
-    document.querySelector('.container').append(newContainer);
+    document.getElementById("main-container").append(newContainer);
     newImg.src = object.image;
+    newImg.id = object.id;
+    newImg.classList.add('serie-img')
     newH2.textContent = object.name;
 };
 
@@ -41,13 +42,17 @@ function getSeriesStyle(arrayObject) {
             styleArray.push(value);
         });
     })
+
+    styleArray.sort();
+
     let styleObject = styleArray.reduce(function (acc, curr) {
         if (acc[curr] == undefined) {
-            acc[curr] = 1;
+            acc[curr] = 0;
         }
         acc[curr] += 1;
         return acc;
     }, {});
+
     return styleObject;
 }
 
@@ -60,6 +65,7 @@ function displaySeriesStyles(object) {
         const newA = document.createElement('a');
 
         newA.classList.add('nav-btn');
+        newA.id = (value);
         newA.textContent = `${value}(${object[value]})`;
         newLi.appendChild(newA);
 
@@ -83,47 +89,73 @@ function getSeriesIdFromStyle(object, style) {
     let idList = [];
     object.forEach((value, index) => {
         if (value.styles.includes(style)) {
-            idList.push({id: value.id})
+            idList.push({ id: value.id })
         }
     })
     return idList;
 }
 
+
 // 8/ Créer une fonction qui souligne le dernier style cliqué.
 // Un seul style doit rester souligné à la fois.
 
-function underlineOnClic() {
+function displaySerieByStyle(object) {
     document.querySelector('#navbar-list').addEventListener('click', function (event) {
-        if (!event.target.classList.contains('nav-btn')) return;
 
+        if (!event.target.classList.contains('nav-btn')) return;
         removeAllUnderlineFromBtn();
-        event.target.classList.add('active-btn')
+        event.target.classList.add('active-btn');
+
+        let seriesByStyle = getSeriesIdFromStyle(object, event.target.id);
+
+        document.querySelectorAll("#container").forEach(function (container) {
+            container.remove();
+        });
+
+        seriesByStyle.forEach(function (value) {
+            object.forEach(function (serie) {
+                displaySerieFromId(serie, value.id);
+            });
+        });
+
     });
 };
 
 // 9/ Créer une fonction pour retirer le soulignement de tous les styles.
 
-function removeAllUnderlineFromBtn(){
+function removeAllUnderlineFromBtn() {
     document.querySelector('#navbar-list li .active-btn').classList.remove('active-btn');
 }
 
 // 10/ Créer une fonction qui affiche dans la page uniquement les séries dont l'id est en paramètre.
 
 function displaySerieFromId(object, identifiant) {
-    
     if (parseInt(identifiant) !== object.id) return;
     displaySerie(object);
-}
+};
 
 // 11/ Modifier la fonction de la question 8/ pour n'afficher que les series du style souligné.
 
 
+
 // 12/ Créer une fonction qui retourne toutes les données d'une série à partir de sont ID.
+
+function getSerieInfoById (object, identifiant) {
+    parseInt(identifiant);
+ 
+    let serie = object.filter(serie => serie.id === identifiant);
+    return serie[0];
+}
 
 
 // 13/ Créer une fonction qui permet qu'au clic sur une série, on affiche son id dans la console.
 
-
+function getIdOnClick(){
+    document.querySelector("#main-container").addEventListener('click', function(event) {
+        if(!event.target.classList.contains('serie-img')) return;
+        console.log(event.target.id);
+    });
+}
 // 14/ Modifier la fonction ci-dessus pour retourner toutes les infos de la serie cliquée dans la console.
 
 
